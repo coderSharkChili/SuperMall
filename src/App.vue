db00bd63f3548cb7bf2c075446512ea569af6745
coderSharkChili <522682190@qq.com>
2021-07-16 14:31:43 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,14 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <router-view></router-view>
   </div>
-  <router-view/>
 </template>
-
+<script>
+export default {
+  name:'App',
+}
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
